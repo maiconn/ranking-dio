d6cb45e4c697ef9f2e1e1f76f95ab25ce4022e5c
Maicon Machado Gerardi da Silva <maicon.gerardi@dbccompany.com.br>
2021-08-02 16:37:31 -0300

    ajustando ranking

--- src/usuario/UsuarioProvider.tsx
+++ src/usuario/UsuarioProvider.tsx
@@ -9,36 +9,33 @@ interface ProviderProps {
 
 export function UsuarioProvider({children}: ProviderProps) {
   const usuariosDIO = [
-    'pro_goncalves',
-    'aronfsouza',
-    'drawklein',
-    'bibianagabriela01',
-    'brunapedagogia15',
-    'denitborges',
-    'pokerzada12',
-    'gabri8014',
-    'gabrieltogni',
-    'gabriele_jacques',
-    'casagrande_contas',
-    'tiago_jb_1',
-    'jairoderivia',
-    'jamsghl',
-    'jardon_m_martins',
-    'joaovitor_jf42',
-    'juroque81',
-    'killianms',
-    'lucas_roquef',
-    'maicon_gerardi',
-    'lyn_mederos',
-    'rafaeladeandrade1',
-    'rafaela_contato2018',
-    'renan_vieira_88',
-    'rodrigorosadossantos',
-    'stephanymberbel1400',
-    'sthefanybarcelosantunes',
-    'wesleymeireles_meireles',
-    'yanfhausmann',
-    'darkmesh'
+    {nome: 'bibianagabriela01', xpDesconto: 160},
+    {nome: 'juroque81', xpDesconto: 40},
+    {nome: 'killianms', xpDesconto: 0},
+    {nome: 'stephanymberbel1400', xpDesconto: 320},
+    {nome: 'lucas_roquef', xpDesconto: 1600},
+    {nome: 'jairoderivia', xpDesconto: 480},
+    {nome: 'pro_goncalves', xpDesconto: 1644},
+    {nome: 'rafaela_contato2018', xpDesconto: 0},
+    {nome: 'pokerzada12', xpDesconto: 1800},
+    {nome: 'gabrieltogni', xpDesconto: 1080},
+    {nome: 'gabriele_jacques', xpDesconto: 0},
+    {nome: 'sthefanybarcelosantunes', xpDesconto: 360},
+    {nome: 'tiago_jb_1', xpDesconto: 400},
+    {nome: 'gabri8014', xpDesconto: 0},
+    {nome: 'brunapedagogia15', xpDesconto: 240},
+    {nome: 'joaovitor_jf42', xpDesconto: 200},
+    {nome: 'rodrigorosadossantos', xpDesconto: 200},
+    {nome: 'renan_vieira_88', xpDesconto: 1920},
+    {nome: 'lyn_mederos', xpDesconto: 80},
+    {nome: 'rafaeladeandrade1', xpDesconto: 40},
+    {nome: 'wesleymeireles_meireles', xpDesconto: 1400},
+    {nome: 'yanfhausmann', xpDesconto: 1320},
+    {nome: 'aronfsouza', xpDesconto: 3600},
+    {nome: 'jamsghl', xpDesconto: 1120},
+    {nome: 'jardon_m_martins', xpDesconto: 4665},
+    {nome: 'denitborges', xpDesconto: 360},
+    {nome: 'maicon_gerardi', xpDesconto: 1004},
   ];
 
   const [{usuarios}, action] = useReducer(usuarioReducer, usuarioInitialState);
@@ -54,10 +51,13 @@ export function UsuarioProvider({children}: ProviderProps) {
     };
     action({name: 'setListUsuario', value: []});
     usuariosDIO.map((usuario) => {
-      fetch(`https://app.digitalinnovation.one/api/users/${usuario}/`, configInit)
+      fetch(`https://app.digitalinnovation.one/api/users/${usuario.nome}/`, configInit)
         .then(res => res.json())
         .then(res => {
           const user = res as Usuario;
+          user.meta.experience = (parseInt(user.meta.experience) - usuario.xpDesconto) + '';
+          //console.log(`{nome: '${usuario.nome}', xpDesconto: ${user.meta.experience}},`)
+
           user.perfil = `https://web.digitalinnovation.one/users/${usuario}`;
           usuarios.push(user);
           usuarios.sort((obj1, obj2) => {
