0dcabd5c49123833ce4f7d83b69dd643844b82d4
Maicon Machado Gerardi da Silva <maicon.gerardi@dbccompany.com.br>
2021-10-08 12:06:49 -0300

    ajustar rank

--- src/usuario/UsuarioProvider.tsx
+++ src/usuario/UsuarioProvider.tsx
@@ -9,35 +9,35 @@ interface ProviderProps {
 
 export function UsuarioProvider({children}: ProviderProps) {
   const usuariosDIO = [
-    {nome: 'jairoderivia', xpDesconto: 480, xpAdd: 0},
-    {nome: 'pro_goncalves', xpDesconto: 3894, xpAdd: 500},
-    {nome: 'wesleymeireles_meireles', xpDesconto: 6690, xpAdd: 0},
-    {nome: 'joaovitor_jf42', xpDesconto: 920, xpAdd: 0},
-    {nome: 'renan_vieira_88', xpDesconto: 2000, xpAdd: 0},
-    {nome: 'bibianagabriela01', xpDesconto: 160, xpAdd: 0},
-    {nome: 'aronfsouza', xpDesconto: 4100, xpAdd: 0},
-    {nome: 'gabriele_jacques', xpDesconto: 80, xpAdd: 0},
-    {nome: 'yanfhausmann', xpDesconto: 1320, xpAdd: 200},
-    {nome: 'rodrigorosadossantos', xpDesconto: 200, xpAdd: 0},
-    {nome: 'matheus_camilo1617', xpDesconto: 1040, xpAdd: 0},
-    {nome: 'lyn_mederos', xpDesconto: 80, xpAdd: 0},
+    {nome: 'matheus_camilo1617', xpDesconto: 1080, xpAdd: 0},
+    {nome: 'jairoderivia', xpDesconto: 520, xpAdd: 0},
     {nome: 'denitborges', xpDesconto: 360, xpAdd: 0},
+    {nome: 'thayroni_spr', xpDesconto: 2320, xpAdd: 0},
+    {nome: 'lyn_mederos', xpDesconto: 80, xpAdd: 0},
+    {nome: 'alissonmanica_aai', xpDesconto: 1122, xpAdd: 0},
+    {nome: 'daniel_monroe_paz', xpDesconto: 280, xpAdd: 0},
+    {nome: 'joaovitor_jf42', xpDesconto: 920, xpAdd: 0},
+    {nome: 'wesleymeireles_meireles', xpDesconto: 7050, xpAdd: 0},
     {nome: 'FRAGAEDUARDO6', xpDesconto: 120, xpAdd: 0},
-    {nome: 'gabrieltogni', xpDesconto: 1160, xpAdd: 700},
-    {nome: 'stephanymberbel1400', xpDesconto: 320, xpAdd: 0},
+    {nome: 'gabrieltogni', xpDesconto: 1200, xpAdd: 0},
+    {nome: 'wagnereduardo34', xpDesconto: 120, xpAdd: 0},
+    {nome: 'gabriele_jacques', xpDesconto: 120, xpAdd: 0},
+    {nome: 'aronfsouza', xpDesconto: 4140, xpAdd: 0},
     {nome: 'tiago_jb_1', xpDesconto: 440, xpAdd: 0},
     {nome: 'rafaela_contato2018', xpDesconto: 0, xpAdd: 0},
-    {nome: 'jardon_m_martins', xpDesconto: 4965, xpAdd: 1400},
-    {nome: 'andregranemann', xpDesconto: 80, xpAdd: 200},
+    {nome: 'guiuser1999', xpDesconto: 5180, xpAdd: 0},
+    {nome: 'yanfhausmann', xpDesconto: 1320, xpAdd: 0},
+    {nome: 'rodrigorosadossantos', xpDesconto: 200, xpAdd: 0},
+    {nome: 'rafael_dapper152', xpDesconto: 1179, xpAdd: 0},
+    {nome: 'schoolartogni', xpDesconto: 600, xpAdd: 0},
+    {nome: 'bibianagabriela01', xpDesconto: 160, xpAdd: 0},
+    {nome: 'andregranemann', xpDesconto: 1240, xpAdd: 0},
+    {nome: 'stephanymberbel1400', xpDesconto: 320, xpAdd: 0},
+    {nome: 'lucas_roquef', xpDesconto: 5120, xpAdd: 0},
+    {nome: 'pro_goncalves', xpDesconto: 4094, xpAdd: 0},
+    {nome: 'jardon_m_martins', xpDesconto: 7345, xpAdd: 0},
     {nome: 'flaviagoudinho39', xpDesconto: 240, xpAdd: 0},
-    {nome: 'lucas_roquef', xpDesconto: 4360, xpAdd: 0},
-    {nome: 'guiuser1999', xpDesconto: 0, xpAdd: 1600},
-    {nome: 'rafael_dapper152', xpDesconto: 0, xpAdd: 0},
-    {nome: 'daniel_monroe_paz', xpDesconto: 0, xpAdd: 0},
-    {nome: 'wagnereduardo34', xpDesconto: 0, xpAdd: 0},
-    {nome: 'thayroni_spr', xpDesconto: 0, xpAdd: 0},
-    {nome: 'alissonmanica_aai', xpDesconto: 0, xpAdd: 700},
-    {nome: 'schoolartogni', xpDesconto: 0, xpAdd: 200},
+    {nome: 'renan_vieira_88', xpDesconto: 2000, xpAdd: 0},
   ];
 
   const [{usuarios}, action] = useReducer(usuarioReducer, usuarioInitialState);
@@ -57,7 +57,7 @@ export function UsuarioProvider({children}: ProviderProps) {
         .then(res => res.json())
         .then(res => {
           const user = res as Usuario;
-          // console.log(`{nome: '${usuario.nome}', xpDesconto: ${user.meta.experience}},`)
+          console.log(`{nome: '${usuario.nome}', xpDesconto: ${user.meta.experience}, xpAdd: 0},`)
           user.meta.experience = (parseInt(user.meta.experience) + usuario.xpAdd - usuario.xpDesconto) + '';
 
           user.perfil = `https://web.digitalinnovation.one/users/${usuario.nome}`;
